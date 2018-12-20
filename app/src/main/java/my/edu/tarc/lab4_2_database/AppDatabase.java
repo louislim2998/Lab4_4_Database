2d53c716c4a078de6ebca7c853c18e7014a5dfa9
Louis Lim <louisl-pa16@student.tarc.edu.my>
2018-12-20 16:37:45 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/lab4_2_database/AppDatabase.java
@@ -0,0 +1,34 @@
+package my.edu.tarc.lab4_2_database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+
+//TODO 5: Create a database class
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+
+    //create an instance of the database
+    private static volatile AppDatabase INSTANCE;
+
+    public abstract UserDao userDao();
+
+    static AppDatabase getDatabase(final Context context){
+        if(INSTANCE == null){
+            synchronized (AppDatabase.class){
+                if(INSTANCE == null){
+                    //Create database here
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class, "user_db")
+                    .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
---
+++ app/src/main/java/my/edu/tarc/lab4_2_database/UserRepository.java
@@ -0,0 +1,47 @@
+package my.edu.tarc.lab4_2_database;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+import java.util.List;
+
+//TODO 6: Create a repository class to manage data query thread
+
+public class UserRepository {
+    private UserDao userDao;
+    private LiveData<List<User>> allUsers;
+
+    public UserRepository(Application application) {
+        //OBTAIN A COPY OF DATABASE
+        AppDatabase db = AppDatabase.getDatabase(application);
+
+        //RETRIEVE THE DAO
+        userDao = db.userDao();
+
+        //RETREIVE ALL THE USER RECORDS
+        allUsers = userDao.loadAllUsers();
+    }
+    
+    LiveData<List<User>> getAllUsers(){
+        return allUsers;
+    }
+    
+    public void insertUser(User user){
+        new insertAsyncTask(userDao).execute(user);
+    }
+
+    //<Param, Progress, Results>
+    private static class insertAsyncTask extends AsyncTask<User, Void, Void> {
+        private UserDao userDao;
+
+        public insertAsyncTask(UserDao userDao) {
+            this.userDao = userDao;
+        }
+
+        @Override
+        protected Void doInBackground(User... users) {
+            userDao.insertUser(users[0]);
+            return null;
+        }
+    }
+}
